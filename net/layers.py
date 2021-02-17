6f47523524f62c468f8285104f7ad8e8d67a0800
Ayush Kumar <ka.kayushskhs24@gmail.com>
2021-02-17 12:08:03 -0500

    Add files via upload

--- net/layers.py
+++ net/layers.py
@@ -8,6 +8,13 @@ def _bn(inputs, is_training):
     )
     return bn
 
+def _conv_nn(inputs, filters, kernel_size, strides=1, padding='same', activation=None, is_training=False, use_bn=True, name = "w99"):
+    filter_ = tf.get_variable(name, shape=[kernel_size[0], kernel_size[1], inputs.shape[-1], filters], initializer=tf.contrib.layers.xavier_initializer())
+    conv = tf.nn.conv2d(inputs, filter_, [1, strides, strides, 1], padding=padding)
+    if activation is not None:
+        conv = activation(conv)
+    return conv
+
 def _conv(inputs, filters, kernel_size, strides=1, padding='same', activation=tf.nn.relu, is_training=False, use_bn=True):
     if use_bn:
         conv = tf.layers.conv2d(
@@ -31,21 +38,21 @@ def _conv(inputs, filters, kernel_size, strides=1, padding='same', activation=tf
         conv = activation(conv)
     return conv
 
-def upsampling(inputs,  method="deconv"):
-    assert method in ["resize", "deconv"]
+def upsampling(inputs, method):
+    #assert method in ["resize", "deconv"]
 
-    if method == "resize":
-        input_shape = tf.shape(inputs)
-        output = tf.image.resize_nearest_neighbor(inputs, (input_shape[1] * 2, input_shape[2] * 2))
+    #if method == "resize":
+    #input_shape = tf.shape(inputs)
+    #output = tf.image.resize_nearest_neighbor(inputs, (input_shape[1] * 2, input_shape[2] * 2))
 
-    if method == "deconv":
-        numm_filter = inputs.shape.as_list()[-1]
-        output = tf.layers.conv2d_transpose(
-            inputs=inputs,
-            filters=numm_filter,
-            kernel_size=4,
-            strides=2,
-            padding='same'
-        )
+    # if method == "deconv":
+    numm_filter = inputs.shape.as_list()[-1]
+    output = tf.layers.Conv2DTranspose(
+        #inputs=inputs,
+        filters=numm_filter,
+        kernel_size=4,
+        strides=2,
+        padding='SAME'
+    ) (inputs)
     return output
 
